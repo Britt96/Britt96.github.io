2a60ad6131227b818d5de962de9e05642517da67
Britt96 <53501455+Britt96@users.noreply.github.com>
2020-12-15 10:24:53 -0500

    Delete graphAnimate.js

--- graphAnimate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/*
-  Assignment: Chapter 5 BMI Tracker App
-  Author: Brittny Eaddy
-  Date: 8/20/2020
-  Purpose: To provide the functionality to preload information in the
-    User Info and Records pages.
-*/
